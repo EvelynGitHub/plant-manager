4a8da22d989eaddc578f01332d3453ed09c358d2
EvelynGitHub <evelynbrandao15@gmail.com>
2021-04-22 20:12:19 -0300

    Adicionando filtragem de plantas por botao de ambienete

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -38,9 +38,19 @@ export function PlantSelect() {
     const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
     const [plants, setPlants] = useState<PlantProps[]>([]);
     const [enviromentSelected, setEnviromentSelected] = useState("all");
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
 
     function handleEnviromentSelected(environment: string) {
         setEnviromentSelected(environment);
+
+        if(environment === 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant => 
+            plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered)
     }
 
     useEffect(() => {
@@ -97,7 +107,7 @@ export function PlantSelect() {
 
             <View style={styles.plants}>
                 <FlatList
-                    data={plants}
+                    data={filteredPlants}
                     renderItem={({item}) => (
                         <PlantCardPrimary data={item} />
                     )}
