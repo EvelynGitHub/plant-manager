196b2c7e7695cb846ce91f20996abd57c4b0be69
EvelynGitHub <evelynbrandao15@gmail.com>
2021-04-23 17:09:18 -0300

    Refatorando e centralizando interface

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -9,32 +9,20 @@ import {
     ActivityIndicator
 } from 'react-native'
 
+import { useNavigation } from '@react-navigation/core'
 import { EnviromentButton } from '../components/EnviromentButton'
 import { Header } from '../components/Header'
 import { PlantCardPrimary } from '../components/PlantCardPrimary'
-import {Load} from '../components/Load'
+import { Load } from '../components/Load'
+import { PlantProps } from '../libs/storage'
 
 import api from '../services/api'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
-import { useNavigation } from '@react-navigation/core'
 
 interface EnviromentProps {
     key: string;
     title: string;
-}
-
-interface PlantProps {
-    id: string;
-    name: string;
-    about: string;
-    water_tips: string;
-    photo: string;
-    environments: [string];
-    frequency: {
-        times: number;
-        repeat_every: string;
-    }
 }
 
 export function PlantSelect() {
