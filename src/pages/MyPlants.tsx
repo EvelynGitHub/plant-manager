6bdca93f4cdcbd16436eaed11df88d40a54c047f
EvelynGitHub <evelynbrandao15@gmail.com>
2021-04-23 19:06:05 -0300

    Adicionado navegaçao em Tabs

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -13,6 +13,8 @@ import { loadPlant, PlantProps } from '../libs/storage'
 import { formatDistance } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { FlatList } from 'react-native-gesture-handler'
+import fonts from '../styles/fonts'
+import { PlantCardSecondary } from '../components/PlantCardSecondary'
 
 
 export function MyPlants() {
@@ -30,9 +32,9 @@ export function MyPlants() {
                 {locale: ptBR}
             )
 
-            setNextWatered(`
-                Não esqueça de regar a ${plantsStoraged[0].name} à ${nextTime} horas.
-            `);
+            setNextWatered(
+                `Não esqueça de regar a ${plantsStoraged[0].name} à ${nextTime} horas.`
+            );
 
             setMyPlants(plantsStoraged)
             setLoading(false)
@@ -61,11 +63,10 @@ export function MyPlants() {
                 <FlatList 
                     data={myPlants}
                     keyExtractor={(item)=> String(item.id)}
-                    renderItem={()=>(
-                        <Text>Elemento</Text>
+                    renderItem={({item})=>(
+                        <PlantCardSecondary data={item}/>
                     )}
                     showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{flex: 1}}
                 />
             </View>
         </View>
@@ -79,18 +80,35 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingHorizontal: 30,
-        paddingTop: 50,
         backgroundColor: colors.background
     },
     spotlight: {
-
+        backgroundColor: colors.blue_light,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        height: 110,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     spotlightImage: {
-
+        width: 60,
+        height: 60,
     },
     spotlightText: {
-
+        flex: 1,
+        color: colors.blue,
+        paddingHorizontal: 10,
+        textAlign: 'justify',
     },
-    plants: {},
-    plantsTitle: {},
+    plants: {
+        flex: 1,
+        width: '100%',
+    },
+    plantsTitle: {
+        fontSize: 24,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        marginVertical: 20,
+    },
 })
