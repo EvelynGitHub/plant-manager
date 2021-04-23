7620edc7740b68e31ed1568e3d31b5ea85664b54
EvelynGitHub <evelynbrandao15@gmail.com>
2021-04-23 12:28:29 -0300

    Pegando nome do usuario pelo asyncStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {
     View,
     Text,
@@ -9,13 +9,25 @@ import {
 import colors from '../styles/colors'
 import userImg from '../assets/userIcon.png'
 import fonts from '../styles/fonts'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export function Header() {
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageUserName() {
+            const user = await AsyncStorage.getItem('@plantmanager:user')
+            setUserName(user || '');
+        }
+
+        loadStorageUserName()
+    }, [userName]);
+
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Olá</Text>
-                <Text style={styles.userName}>Nome</Text>
+                <Text style={styles.userName}>{userName}</Text>
             </View>
             <Image source={userImg} style={styles.image} />
         </View>
